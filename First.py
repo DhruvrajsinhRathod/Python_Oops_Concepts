b150337aed5da7e85c48ba95c04da216c225628d
Dhruv <ddrathod510@gmail.com>
2024-11-15 20:14:54 -0500

    another first commit

--- First.py
+++ First.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+class Dhruvraj:
+    a = 10
+    b = 20
+    print("a is : " + str(a))
+    print("b is : " + str(b))
+
+    def __init__(self):
+        print("Init Function is called!")
+
+    def methodDhruv(i):
+        print("Dhruv is called" + i)
+
+
+d1 = Dhruvraj()
+Dhruvraj.methodDhruv("Hi")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+a = 10
+    b = 20
+    print("a is : " + str(a))
+    print("b is : " + str(b))
